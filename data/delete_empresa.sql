597a5c07a76623cf659a4e462e689c3737c9166f
Rayssa Fialho <lrayssa232@gmail.com>
2026-03-29 15:41:54 -0300

    Add SQL script to delete specific employees

---
+++ data/delete_empresa.sql
@@ -0,0 +1,9 @@
+use empresa;
+delete from funcionario
+where codigo = 2;
+delete from funcionario
+where codigo = 5;
+delete from funcionario
+where codigo = 1;
+delete from funcionario
+where codigo = 17;
